b6d7417a4b742ab2d29636886ef4b2da8574e2ba
ertechin <erselertekin@hotmail.com>
2025-05-17 16:53:19 +0300

    Reactive attributes

--- stop_watch.py
+++ stop_watch.py
@@ -1,16 +1,37 @@
+from time import monotonic
+
 from textual.app import App, ComposeResult
 from textual.containers import HorizontalGroup, VerticalScroll
+from textual.reactive import reactive
 from textual.widgets import Button, Digits, Footer, Header
 
 
 class TimeDisplay(Digits):
     """A widget to display elapsed time."""
 
+    start_time = reactive(monotonic)
+    time = reactive(0.0)
+
+    def on_mount(self) -> None:
+        """Event handler called when widget is added to the app."""
+        self.set_interval(1 / 60, self.update_time)
+
+    def update_time(self) -> None:
+        """Method to update the time to the current time."""
+        self.time = monotonic() - self.start_time
+
+    def watch_time(self, time: float) -> None:
+        """Called when the time attribute changes."""
+        minutes, seconds = divmod(time, 60)
+        hours, minutes = divmod(minutes, 60)
+        self.update(f"{hours:02,.0f}:{minutes:02.0f}:{seconds:05.2f}")
+
 
 class Stopwatch(HorizontalGroup):
     """A stopwatch widget."""
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
+        """Event handler called when a button is pressed."""
         if event.button.id == "start":
             self.add_class("started")
         elif event.button.id == "stop":
@@ -21,11 +42,12 @@ class Stopwatch(HorizontalGroup):
         yield Button("Start", id="start", variant="success")
         yield Button("Stop", id="stop", variant="error")
         yield Button("Reset", id="reset")
-        yield TimeDisplay("00:00:00.00")
+        yield TimeDisplay()
 
 
 class StopwatchApp(App):
     """A Textual app to manage stopwatches."""
+
     CSS_PATH = "stop_watch.tcss"
     BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
 
